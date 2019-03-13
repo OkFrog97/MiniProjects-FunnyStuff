2c743aa901c00a6b46437acf475103ea2b2ae95a
OkForg97 <okfrog97@yandex.ru>
2019-03-13 08:11:07 +0500

    add new classes

--- BlackJack_cardgame.py
+++ BlackJack_cardgame.py
@@ -1,13 +1,22 @@
-import BaseCardsClasses_from_Douson_book, PlayerClass_from_Douson_book
+import BaseCardsClasses_from_Douson_book as cards 
 
-class BJ_card (BaseCardsClasses_from_Douson_book.card):
+import PlayerClass_from_Douson_book as games
+
+class BJ_Card (cards.Card):
     ACE_VALUE = 1
     @property
     def value (self):
         if self.is_face_up: #if True
-            v = BJ_card.RANNS.index(self.rank) + 1
+            v = BJ_Card.RANKS.index(self.rank) + 1
             if v > 10:
                 v = 10
         else:
             v = None
+        return V
 
+class BJ_Deck (cards.Hand):
+    '''BlackJack deck'''
+    def populate (self):
+        for suit in BJ_Card.SUITS:
+            for rank in BJ_Card.RANKS:
+                self.cards.append(BJ_Card(rank, suit))
