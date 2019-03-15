89c35fe4b70d5c0e6090b4398db58fa0c668de93
OkForg97 <okfrog97@yandex.ru>
2019-03-15 07:28:34 +0500

    Try without add

--- BlackJack_cardgame.py
+++ BlackJack_cardgame.py
@@ -25,4 +25,16 @@ class BJ_Hand (cards.Hand):
     def __init__(self, name):
         cards.Hand.__init__(self) #In book: super(BJ_Hand, self).__init__()
         self.name = name
+    def __str__(self):
+        rep = self.name + ':\t' + super((BJ_Hand, self).__str__()
+        if self.total:
+            rep += '({})'.format(str(self.total))
+        return rep
+    @property
+    def total(self):
+        #if one of card value = None then all values = None; Чего, блеать?
+        for card in self.cards:
+            in not card.value:
+                return None
+        #Sum points, Ace = 1;
         
