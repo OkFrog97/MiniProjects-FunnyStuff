020bacdbd684b069a41ecab52df3e4e9ec1aed95
OkForg97 <okfrog97@yandex.ru>
2019-03-20 06:03:39 +0500

    Божеский вид

--- Credit_Calc.py
+++ Credit_Calc.py
@@ -1,24 +1,21 @@
-# RU:Программа расчета платежей по кредиту; EN: Program for calculation pay of credit;
+'''
+RU:Программа расчета платежей по кредиту; EN: Program for calculation pay of credit;
+'''
 
-# var
+def Credit_Calc (credit, precent, time):
+    month = time * 12 # yers to month;
+    x = credit / month # month credit;
+    y = (credit / 100 * precent) / 12 # month precent;
+    z = x + y # month pay;
+    overpall = z * month
+    return z, x, y, overpall 
 
-credit = float (input('Введите сумму кредита ')) # RU:Сумма кредита; EN: Amount of credit;
-precent = float (input('Введите сумма процента годовых ')) # RU:Сумма процентов; EN: Interest credit;
-time = float (input ('Введите срок кредита в годах ')) # RU:Срок кредита в годах; EN: Credit term;
-print ()
+def main ():
+    credit = float (input('Введите сумму кредита ')) # RU:Сумма кредита; EN: Amount of credit;
+    precent = float (input('Введите сумма процента годовых ')) # RU:Сумма процентов; EN: Interest credit;
+    time = float (input ('Введите срок кредита в годах ')) # RU:Срок кредита в годах; EN: Credit term;
+    print('Ежемесячный платеж составляет {0};\nИз которых сумма основного долга {1};\nЕжемесячный платеж по процентам {2};\nВсего переплата составит {3}.'.format(Credit_Calc (credit, precent, time)))
+    input()
 
-
-month = time * 12 # yers to month;
-
-x = credit / month # month credit;
-
-y = (credit / 100 * precent) / 12 # month precent;
-
-z = x + y # month pay;
-
-print ('Ежемесячный платеж составляет ', round (z, 2), ';','\nИз которых сумма основного долга ', round (x, 2), ';','\nЕжемесячный платеж по процентам', round (y, 2), ';\n')
-print ('Всего переплата составит ', y * month, ';\n')
-
-input ()
-
-# end
+if __name__ == '__main__':
+    main()
