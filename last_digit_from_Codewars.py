a59f33211c4ee0bcde46458765a7fcc326d11507
OkForg97 <okfrog97@yandex.ru>
2019-03-22 07:10:38 +0500

    try fix the bug

--- last_digit_from_Codewars.py
+++ last_digit_from_Codewars.py
@@ -1,5 +1,5 @@
 def last_digit(n1, n2):
-   '''
+    '''
     Task from codewars.
     n1 is a number. n2 is elevating number.
     Return last digit of elevating.
@@ -10,7 +10,11 @@ def last_digit(n1, n2):
     '''
     if n2 == 0:
         return 1
-    elif int(str(n1)[len(str(n1))-1]) in [0,1,5]: #if last n1 digit in list return it cose answer is not change.
+    
+    elif int(str(n1)[len(str(n1))-1]) in [0, 1, 5, 6] or n2 == 1: #if last n1 digit in list return it cose answer is not change.
         return int(str(n1)[len(str(n1))-1])
     else:
         return int(str(n1**(n2%4))[len(str(n1**(n2%4)))-1]) #Remainder of division is elevating number for n1 (or last n1 digital) which can show last n1**n2 digital.
+    
+if __name__ == '__main__':
+    main()
