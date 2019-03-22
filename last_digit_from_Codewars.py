6f5513c110bbd50d30779bd5257bb7b6ac9c5b03
OkForg97 <okfrog97@yandex.ru>
2019-03-22 06:16:56 +0500

    1 should be 6 bug

--- last_digit_from_Codewars.py
+++ last_digit_from_Codewars.py
@@ -1,5 +1,16 @@
 def last_digit(n1, n2):
-    
-    digits = [int(str(int(str(n1)[len(str(n1))-1])**i)[len(str(n1**i))-1]) for i in range (1, 5)]
-    
-    return digits[n2%len(set(digits))-1]
+   '''
+    Task from codewars.
+    n1 is a number. n2 is elevating number.
+    Return last digit of elevating.
+    >last_digit (2, 3) # 2**3=8;
+    >8
+    >last_digit (5, 3) # 5**3=125;
+    >5
+    '''
+    if n2 == 0:
+        return 1
+    elif int(str(n1)[len(str(n1))-1]) in [0,1,5]: #if last n1 digit in list return it cose answer is not change.
+        return int(str(n1)[len(str(n1))-1])
+    else:
+        return int(str(n1**(n2%4))[len(str(n1**(n2%4)))-1]) #Remainder of division is elevating number for n1 (or last n1 digital) which can show last n1**n2 digital.
