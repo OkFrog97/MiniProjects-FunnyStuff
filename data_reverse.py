6b555775dc5f3c66eae9989510a45183c1cd8c1c
OkForg97 <okfrog97@yandex.ru>
2019-03-23 17:10:13 +0500

    Add test

--- data_reverse.py
+++ data_reverse.py
@@ -4,15 +4,17 @@ def data_reverse(data):
     '''
     answ = []
     for i in range (len(data)-1,0,-4):
-	    for i in range (i-3,i+1):
-		    answ.append (data[i])
+	    for j in range (i-3,i+1):
+		    answ.append (data[j])
     return answ
 
 def tests():
-    pass
+    data1 = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+    data2 = [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
+    print (data_reverse(data1)==data2)
 
 def main():
-    pass
+    tests()
 
 if __name__ == '__main__':
     main()
