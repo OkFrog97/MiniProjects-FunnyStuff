e1f31dc6997be5671a1b8df11d9affa827a73116
OkForg97 <okfrog97@yandex.ru>
2019-03-23 19:53:41 +0500

    War card game init

---
+++ Card_games_OOP/WarCard.py
@@ -0,0 +1,10 @@
+import BaseCardsClasses_from_Douson_book as cards 
+
+from PlayerClass_from_Douson_book import Player as unit
+
+class War_Deck (cards.Deck): 
+    '''
+    Deck for War card geming. Import from cards.
+    Колода для карточной игры Война. Импортируется из cards.
+    '''
+    
