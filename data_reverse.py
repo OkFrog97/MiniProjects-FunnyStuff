ca0025326006bd8facf2ae534d853df19ede7429
OkForg97 <okfrog97@yandex.ru>
2019-03-23 17:18:11 +0500

    Test passed

--- data_reverse.py
+++ data_reverse.py
@@ -3,15 +3,16 @@ def data_reverse(data):
     This program revers 4-bits data.
     '''
     answ = []
-    for i in range (len(data)-1,0,-4):
-	    for j in range (i-3,i+1):
-		    answ.append (data[j])
+    for i in range (len(data)-1,0,-8):
+        for j in range (i-7,i+1):
+            answ.append (data[j])
     return answ
 
 def tests():
     data1 = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
     data2 = [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
     print (data_reverse(data1)==data2)
+    input()
 
 def main():
     tests()
