7ac80c925d6b33b70a9550477fbb365cc070e0d4
OkForg97 <okfrog97@yandex.ru>
2019-03-29 18:21:07 +0500

    Cata 6 Diamond pass test

--- Diamonds.py
+++ Diamonds.py
@@ -11,6 +11,9 @@ def diamond (n):
     else:
         answer = ""
         for i in range (1,n+1,2):
-	        answer += (" "*((n-i)//2) + i*'*' + " "*((n-i)//2)+'\n')
-        for i in range (n-1,1,-2):
-            answer += (" "*((n-i)//2) + i*'*' + " "*((n-i)//2)+'\n')
+	        answer += (" "*((n-i)//2) + i*'*' + '\n')
+        
+        for i in range (n-2,0,-2):
+            answer += (" "*((n-i)//2) + i*'*'+'\n')
+        
+        return answer
