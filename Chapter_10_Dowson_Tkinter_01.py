804ebddd3148ff367721fd86538df80458157787
OkForg97 <okfrog97@yandex.ru>
2019-04-02 07:28:27 +0500

    add label

--- Chapter_10_Dowson_Tkinter_01.py
+++ Chapter_10_Dowson_Tkinter_01.py
@@ -10,7 +10,11 @@ root = Tk () #Base window
 #Window modification
 root.title("Тирлим-бом-бом")
 root.geometry("500x300")
+app = Frame(root) # It is ramochka (frame)
+app.grid()
+
+lbl = Label(app, text ="Клянусь своим дуратским лбом!") #Creating МЕТКА inside frame
+lbl.grid()
 
 #Event-loop star
 root.mainloop()
-
