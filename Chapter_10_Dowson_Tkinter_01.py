046841c1fcf17f4d4bc97525b7c84295bfbf8bc6
OkForg97 <okfrog97@yandex.ru>
2019-04-02 07:02:43 +0500

    It's work

--- Chapter_10_Dowson_Tkinter_01.py
+++ Chapter_10_Dowson_Tkinter_01.py
@@ -3,10 +3,14 @@ Chapter 10. GUI.
 Programm 1. Easy Tkinter GUI.
 '''
 
-from Tkinter import *
+from tkinter import *
 
 root = Tk () #Base window
 
 #Window modification
+root.title("Тирлим-бом-бом")
+root.geometry("500x300")
 
+#Event-loop star
+root.mainloop()
 
