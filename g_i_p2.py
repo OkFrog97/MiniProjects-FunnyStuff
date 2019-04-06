bd422d68c1fb9769d856b21fb2db01688dd4623c
OkForg97 <okfrog97@yandex.ru>
2019-04-06 22:00:10 +0500

    another gap alg done

--- g_i_p2.py
+++ g_i_p2.py
@@ -1,27 +1,29 @@
 def gap(g, m, n):
-    '''
-    find gap = g in lots of m and n.
-    More effective way.
-    '''
-    prime_nums=[2]
-    for i in range(3, n+1, 2):
-	    if (i > 10) and (i%10==5):
-		    continue
-	    for j in prime_nums:
-		    if j*j-1 > i:
-			    prime_nums.append(i)
-			    break
-		    if (i % j == 0):
-			    break
-	    else:
-		    prime_nums.append(i)
-
-    for k in range(m, len(prime_nums)-1): #Find our g
-        if  (prime_nums[k+1]-prime_nums[k]) == g:
-            return [prime_nums[k], prime_nums[k+1]]
-   
+    start = 0
+    end = 0
+    for i in range(m,n+1):
+      if is_prime(i):
+        if start == 0:
+          start = i
+        elif end == 0:
+          end = i
+        else:
+          start = end
+          end = i
+      if end - start == g:
+        return [start, end]
     return None
-    
+
+
+def is_prime(n):
+  if n <= 0 or n == 1:
+    return False
+  i = 2
+  while (i <= n ** 0.5 ):
+    if n % i == 0:
+      return False
+    i += 1
+  return True
     
     
 def main ():
