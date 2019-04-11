7f90c1eef98792b35901be6c507ca3d6998a6d76
OkForg97 <okfrog97@yandex.ru>
2019-04-11 08:43:33 +0500

    add base app and text instructions

--- GUI_Secret_Dowson.py
+++ GUI_Secret_Dowson.py
@@ -2,3 +2,20 @@
 This programm say how you can living a long time.
 '''
 
+from tkinter import *
+
+class Application (Frame):
+    '''
+    GUI-application.
+    '''
+    def __init__(self, master):
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widget()
+    
+    def create_widget(self):
+        #text instruction
+        self.inst_lbl = Lable (self, text = "Введите пароль \"secret\" что-бы узнать секрет долголетия: ")
+        self.init_lbl.grid(row = 0, column = 0, columnspan = 2, sticky = W)
+        self.pw_lbl = Lable(self, text = "Пароль")
+        self.pw_lbl.grid(row = 1, column = 0, sticky = W)
