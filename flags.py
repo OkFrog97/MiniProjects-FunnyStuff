798570e3c39522bf11efdf4756729548d622150d
OkForg97 <okfrog97@yandex.ru>
2019-04-14 16:18:01 +0500

    Add basic classes

--- flags.py
+++ flags.py
@@ -9,6 +9,14 @@ class Application (Frame):
         self.create_widgets()
         
     def create_widgets(self):
-        
+        Label(self, text = "Укажите ваши любимые жанры кино").grid(row = 0, column = 0, sticky = W)
+        Lable (Self, text = "Выберите все, что вам по вкусу").grid(row = 1, column = 0, sticky = W)
+        #comedy
+        self.likes_comedy = BoolenVar()
+        Checkbutton(self, text = "Комедия", variable=self.likes_comedy, command = self.update_text).grid(row = 2, column = 0, stick = W)
+        #horrors
+        Checkbutton(self, text = "Ужасы", variable=self.likes_comedy, command = self.update_text).grid(row = 3, column = 0, stick = W)
+        #romatic
+        Checkbutton(self, text = "Фильмы о любви", variable=self.likes_comedy, command = self.update_text).grid(row = 4, column = 0, stick = W)
         
 
