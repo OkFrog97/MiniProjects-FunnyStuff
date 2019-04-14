862c915298a932ea23a61f8804a13f09f72b4b7d
OkForg97 <okfrog97@yandex.ru>
2019-04-14 16:03:03 +0500

    New prgramm: Flags

---
+++ flags.py
@@ -0,0 +1,14 @@
+#Flags programm
+
+from tkinter import *
+
+class Application (Frame):
+    def __init__ (self, master):
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widgets()
+        
+    def create_widgets(self):
+        
+        
+
