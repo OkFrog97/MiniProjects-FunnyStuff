80a3ca552661d750c02afdd9e36bf6ce4d28f94f
OkForg97 <okfrog97@yandex.ru>
2019-04-15 20:49:51 +0500

    Programm is done

--- flags_wiith_dots.py
+++ flags_wiith_dots.py
@@ -11,33 +11,33 @@ class Application (Frame):
     def create_widgets(self):
         Label(self, text = "Укажите ваши любимые жанры кино").grid(row = 0, column = 0, sticky = W)
         Label (self, text = "Выберите один").grid(row = 1, column = 0, sticky = W)
-        salf.favorite = StringVar ()
-        self.favorite.sat(None)
+        self.favorite = StringVar ()
+        self.favorite.set(None)
         Radiobutton(self,
                     text = "Comedy",
-                    variable = salf.favorite,
-                    value = "Comedy"
+                    variable = self.favorite,
+                    value = "Comedy",
                     command = self.update_text
                     ).grid(row=2, column=0, sticky = W)
         Radiobutton(self,
                     text = "Horror",
-                    variable = salf.favorite,
-                    value = "Horror"
+                    variable = self.favorite,
+                    value = "Horror",
                     command = self.update_text
                     ).grid(row=3, column=0, sticky = W)
         Radiobutton(self,
                     text = "Romantic",
-                    variable = salf.favorite,
-                    value = "Romantic"
+                    variable = self.favorite,
+                    value = "Romantic",
                     command = self.update_text
                     ).grid(row=4, column=0, sticky = W)
-        self.result_txt = Text(self, width = 40, height = 5, wrap = WORD)
-        self.result_txt.grid(row=5, column=0, columnspan = 3)
+        self.result_text = Text(self, width = 40, height = 5, wrap = WORD)
+        self.result_text.grid(row=5, column=0, columnspan = 3)
         
     def update_text(self):
-        message = "Your favorite films is {}".format(self.favorite.get())
+        message = "Your favorite films is {}.".format(self.favorite.get())
         self.result_text.delete(0.0, END)
-        self.result_text.insert(0.0, likes)
+        self.result_text.insert(0.0, message)
 
 
         
