d3b9e6bd2559f6eac0535053e89e9e08e0c2475e
OkForg97 <okfrog97@yandex.ru>
2019-04-19 06:37:06 +0500

    Add widgets

--- Home_work_chap_10_guess.py
+++ Home_work_chap_10_guess.py
@@ -13,7 +13,35 @@ class Application (Frame):
     
     def creat_widgets (self):
         '''Creating widgets.'''
+        #Discription
         Label  (self,
                 text = 'Я загадал число. Попытайтесь его угадать и ввести в полне ниже.'
                 ).grid(row = 0, column = 0, columnspan = 2, sticky = W)
+        #Area for user input
+        self.user_ent = Entry(self)
+        self.user_ent.grid(row = 1, column = 0, sticky = W)
+        #Starting bunnot
+        Button(self,
+               text = "Угадать число.",
+               command = self.gess_number
+               ).grid(row = 2, column = 1, sticky = W)
+        #Text area.
+        self.story_text = Text (self, width = 75, height = 10, wrap = WORD)
+        self.story_text.grid(row = 3, column = 0, columnspan = 4)
+
+
+
+
+
+
+
+
         
+def main ():
+    root = Tk()
+    root.title('Угадай число')
+    app = Application (root)
+    root.mainloop()
+ 
+if __name__ == '__main__':
+    main()
