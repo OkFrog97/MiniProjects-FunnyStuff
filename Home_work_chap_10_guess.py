500b199d8f75231afea63f4e6a127afba33047e7
OkForg97 <okfrog97@yandex.ru>
2019-04-19 06:55:05 +0500

    programm is done

--- Home_work_chap_10_guess.py
+++ Home_work_chap_10_guess.py
@@ -15,7 +15,7 @@ class Application (Frame):
         '''Creating widgets.'''
         #Discription
         Label  (self,
-                text = 'Угадай число от 1 до 100. Для ввода используй поле.'
+                text = 'Угадай число от 1 до 10. Для ввода используй поле.'
                 ).grid(row = 0, column = 0, columnspan = 2, sticky = W)
         #Area for user input
         self.user_ent = Entry(self)
@@ -26,18 +26,40 @@ class Application (Frame):
                command = self.gess_number
                ).grid(row = 2, column = 0, sticky = W)
         #Text area.
-        self.story_text = Text (self, width = 40, height = 5, wrap = WORD)
-        self.story_text.grid(row = 3, column = 0, sticky = W)
+        self.answer_text = Text (self, width = 40, height = 5, wrap = WORD)
+        self.answer_text.grid(row = 3, column = 0, sticky = W)
 
+    def say_answer (self, text):
+        '''
+        Clean text area and print text.
+        '''
+        self.answer_text.delete(0.0, END)
+        self.answer_text.insert(0.0, text)
+    
+    
     def gess_number (self):
-        pass
-
-
-
-
-
-
+        '''
+        Pick number function.
+        '''
+        import random  
         
+        the_number = random.randint (1, 10)
+        guess = self.user_ent(get)
+        
+        while guess != the_number:
+            if guess > the_number:
+                answer = "Меньше"
+                say_answer(answer)
+            else:
+                answer = "Больше"
+                say_answer(answer)
+        
+        answer = "Ты угадал! Загаданное число - {}.".format(the_number)
+        say_answer(answer)
+  
+
+
+
 def main ():
     root = Tk()
     root.title('Угадай число')
