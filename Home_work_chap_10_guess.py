75505af0ef1a9430b3cf522ca90b3cf0cfafb8ca
OkForg97 <okfrog97@yandex.ru>
2019-04-19 06:44:13 +0500

    Front-end fixing

--- Home_work_chap_10_guess.py
+++ Home_work_chap_10_guess.py
@@ -15,7 +15,7 @@ class Application (Frame):
         '''Creating widgets.'''
         #Discription
         Label  (self,
-                text = 'Я загадал число. Попытайтесь его угадать и ввести в полне ниже.'
+                text = 'Угадай число от 1 до 100. Для ввода используй поле.'
                 ).grid(row = 0, column = 0, columnspan = 2, sticky = W)
         #Area for user input
         self.user_ent = Entry(self)
@@ -24,12 +24,13 @@ class Application (Frame):
         Button(self,
                text = "Угадать число.",
                command = self.gess_number
-               ).grid(row = 2, column = 1, sticky = W)
+               ).grid(row = 2, column = 0, sticky = W)
         #Text area.
-        self.story_text = Text (self, width = 75, height = 10, wrap = WORD)
-        self.story_text.grid(row = 3, column = 0, columnspan = 4)
+        self.story_text = Text (self, width = 40, height = 5, wrap = WORD)
+        self.story_text.grid(row = 3, column = 0, sticky = W)
 
-
+    def gess_number (self):
+        pass
 
 
 
