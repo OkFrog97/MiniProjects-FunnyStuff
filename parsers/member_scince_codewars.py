3c2e91f9a62d2415ff25ec7b4b3fbe186c5a0c47
OkForg97 <okfrog97@yandex.ru>
2019-04-27 17:24:20 +0500

    site and tags tree done

--- parsers/member_scince_codewars.py
+++ parsers/member_scince_codewars.py
@@ -1,12 +1,20 @@
-def get_member_sience():
+def get_member_sience(username):
    '''
    Return since data of member connectione to Codewars.
    '''
    #import modules
    import requests
    import bs4
+   
    #get site and create tags tree
+   username_path = "https://www.codewars.com/users/{}".format(username)
+   site = requests.get()
+   tree_site = bs4.BeautifulSoup (site.text, "html.parser")
+   
    #catch errors
+   
    #find connection date
+   
    #sad answer
+   
    pass
