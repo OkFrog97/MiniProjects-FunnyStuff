52def4509fdcf5057c47650d4f3cf0988a5e7077
OkForg97 <okfrog97@yandex.ru>
2019-04-27 16:10:44 +0500

    Add basic commets(map)

---
+++ parsers/member_scince_codewars.py
@@ -0,0 +1,12 @@
+def get_member_sience():
+   '''
+   Return since data of member connectione to Codewars.
+   '''
+   #import modules
+   import requests
+   import bs4
+   #get site and create tags tree
+   #catch errors
+   #find connection date
+   #sad answer
+   pass
