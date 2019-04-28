77017ac933d958c8b5e6e36e425543fafdc2c43a
OkForg97 <okfrog97@yandex.ru>
2019-04-28 09:50:40 +0500

    fix bug

--- parsers/member_scince_codewars.py
+++ parsers/member_scince_codewars.py
@@ -1,33 +1,33 @@
 def get_member_sience(username):
-   '''
-   Return since data of member connectione to Codewars.
-   > Usernames: Fred, John, Michel.
-   > get_member_sience (Fred)
-   > Oct 2018
-   >
-   > get_member_sience (John)
-   > Jun 2017
-   >
-   > get_member_sience (Michel)
-   > September 2015
-   '''
-   #import modules
-   import requests
-   import bs4
+    '''
+    Return since data of member connectione to Codewars.
+    > Usernames: Fred, John, Michel.
+    > get_member_sience (Fred)
+    > Oct 2018
+    >
+    > get_member_sience (John)
+    > Jun 2017
+    >
+    > get_member_sience (Michel)
+    > September 2015
+    '''
+    #import modules
+    import requests
+    import bs4
+    
+    #get site and create tags tree
+    username_path = "https://www.codewars.com/users/{}".format(username)
+    site = requests.get()
+    tree_site = bs4.BeautifulSoup (site.text, "html.parser")
+    
+    #find connection date
+    stat = tree_site.select('.stat-box .stat')
+    reg_date = stat[3].getText()
+    
+    #catch errors
+    if stat == None:
+        return "Username wasn't registrated."
    
-   #get site and create tags tree
-   username_path = "https://www.codewars.com/users/{}".format(username)
-   site = requests.get()
-   tree_site = bs4.BeautifulSoup (site.text, "html.parser")
-   
-   #find connection date
-   stat = tree_site.select('.stat-box .stat')
-   reg_date = stat[3].getText()
-   
-   #catch errors
-   if stat == None:
-      return "Username wasn't registrated."
-   
-   #sed answer
-   answer = reg_date[13:]
-   return answer
+    #sed answer
+    answer = reg_date[13:]
+    return answer
