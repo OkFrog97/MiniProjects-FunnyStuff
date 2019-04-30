2608ebe74ed142f7c3fcbb7d873f531c253eab39
OkForg97 <okfrog97@yandex.ru>
2019-04-30 07:02:28 +0500

    fixed all bugs

--- socks_in_basket.py
+++ socks_in_basket.py
@@ -2,15 +2,15 @@
 Let's find same socks in basket with help of labda function.
 basket is list of socks where same colors is a same numbers.
 Example:
->baksket = [10, 20, 30, 10]
+>basket = [10, 20, 30, 10]
 >socks_finder(basket)
 >1
 >
->baksket = [40,50,70,70,30,50]
+>basket = [40,50,70,70,30,50]
 >socks_finder(basket)
 >2
 >
->baksket = [40. 20, 10]
+>basket = [40. 20, 10]
 >socks_finder(basket)
 >0
 >
@@ -18,12 +18,12 @@ Example:
 
 socks_finder=lambda basket: sum([basket.count(i)//2 for i in set(basket)])
 
-def main:
-    baksket = [10, 20, 30, 10]
+def main ():
+    basket = [10, 20, 30, 10]
     print(socks_finder(basket))
-    baksket = [40,50,70,70,30,50]
+    basket = [40,50,70,70,30,50]
     print(socks_finder(basket))
-    baksket = [40. 20, 10]
+    basket = [40, 20, 10]
     print(socks_finder(basket))
     
 if __name__ == "__main__":
