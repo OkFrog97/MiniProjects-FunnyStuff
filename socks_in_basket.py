1e6e580cca8b4bea68107af243b5c0382370a168
OkForg97 <okfrog97@yandex.ru>
2019-04-30 07:00:55 +0500

    add main function

--- socks_in_basket.py
+++ socks_in_basket.py
@@ -18,3 +18,14 @@ Example:
 
 socks_finder=lambda basket: sum([basket.count(i)//2 for i in set(basket)])
 
+def main:
+    baksket = [10, 20, 30, 10]
+    print(socks_finder(basket))
+    baksket = [40,50,70,70,30,50]
+    print(socks_finder(basket))
+    baksket = [40. 20, 10]
+    print(socks_finder(basket))
+    
+if __name__ == "__main__":
+    main()
+    
