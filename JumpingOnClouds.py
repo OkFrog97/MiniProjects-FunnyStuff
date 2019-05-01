a34eec872778e401ad741e9a2140f1434b81226f
OkForg97 <okfrog97@yandex.ru>
2019-05-01 20:24:46 +0500

    JunpingOnClouds

---
+++ JumpingOnClouds.py
@@ -0,0 +1,42 @@
+def jumpingOnClouds(c):
+    '''
+    Return minimal player's jumping on clouds.
+    C - is list of clouds where 0 is good cloud and 1 - danger cloud.
+    Player can jump on first or second cloud before him.
+    Example:
+    >jumpingOnClouds([0,0,0,1,0,1,0,0])
+    >4
+    >
+    >jumpingOnClouds([0,1,0,0,1,0])
+    >3
+    '''
+    
+    #add vars
+    player = 0 #player position on clouds
+    jumps = 0
+    last_cloud = len(c)
+    
+   
+    #find minimal jumps
+    while player != last_cloud:
+        if c[player+2] != 1 and player+2 <= last_cloud:
+            player += 2
+        else:
+            player += 1
+        jump += 1   
+            
+    #return result
+    return jumps
+
+def main()
+    print (jumpingOnClouds([0,0,0,1,0,1,0,0]))
+    print ('Answer: 4')
+    print (jumpingOnClouds([0,1,0,0,1,0]))
+    print ('Answer: 3')
+    print (jumpingOnClouds([0,0,1,0,0,1,0]))
+    print ('Answer: 4')
+    print (jumpingOnClouds([0,0,0,1,0,0]))
+    print ('Answer: 3')
+    
+if __name__ == "__main__":
+    main()
