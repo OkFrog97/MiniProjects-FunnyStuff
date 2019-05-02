d3207a46c9b3415b06a4702d15c6ffd3c9a06b33
OkForg97 <okfrog97@yandex.ru>
2019-05-02 22:30:13 +0500

    ya v govno

--- repeatedString.py
+++ repeatedString.py
@@ -1,1 +1,11 @@
 def repeatedString(s, n):
+    a = s.count("a")
+    answer = 0
+    
+    answer = a * (n//len(s))
+    
+    for i in range(n%len(s)):
+        if s[i] == 'a':
+            answer += 1
+    
+    return answer
