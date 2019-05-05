9f38c92b25b40daa8c94b5ccd4757afc4497a3ec
OkForg97 <okfrog97@yandex.ru>
2019-05-05 22:28:42 +0500

    tests test

---
+++ dytest.py
@@ -0,0 +1,15 @@
+def dynamictest():
+    print ('This is dynamik test')
+    print ('Test is begining')
+    print ('Three')
+    print ('Twoo')
+    print ('Wan')
+    print ('Go!')
+    print ('\a')
+    print ('\a\a\a\a')
+    print ('\a')
+    print ('the End\nwriten by FreddyOkFrog97')
+    input ('Press enter for escape')
+    return
+
+dynamictest ()
