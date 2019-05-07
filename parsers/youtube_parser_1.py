36584b384a0ecb3ab7b23a834449b180455ecf88
OkForg97 <okfrog97@yandex.ru>
2019-05-07 07:27:05 +0500

    Lets start a war

---
+++ parsers/youtube_parser_1.py
@@ -0,0 +1,28 @@
+#1/usr/bin/evn python3
+
+#import using libs
+import requests
+import bs4
+
+def get_html(url):
+    '''
+    return html text.
+    '''
+    return requests.get(url).text
+
+
+
+
+
+
+
+
+
+
+
+
+def main ():
+    pass
+    
+if __name__ == "__main__":
+    main ()
