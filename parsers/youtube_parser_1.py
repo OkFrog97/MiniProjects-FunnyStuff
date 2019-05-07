850bbee6bfc0c6c7fc742f3e2214ca9228542323
OkForg97 <okfrog97@yandex.ru>
2019-05-07 07:40:25 +0500

    Start a nuclear war

--- parsers/youtube_parser_1.py
+++ parsers/youtube_parser_1.py
@@ -1,15 +1,26 @@
 #1/usr/bin/evn python3
 
+'''
+This programm is parser for http://www.weblancer.net
+'''
+
 #import using libs
 import requests
 import bs4
 
+
 def get_html(url):
     '''
-    return html text.
+    Return html as text.
     '''
     return requests.get(url).text
 
+
+def parser (html):
+    '''
+    Take html and return seatchng data.
+    '''
+    soup = sp4.BeautifulSoup(html)
 
 
 
