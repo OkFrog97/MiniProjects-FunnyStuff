f0f298816ced4b040efb3f21d1b56e6c8a46b9c8
OkForg97 <okfrog97@yandex.ru>
2019-05-11 09:42:36 +0500

    pre-qwe

--- countingValleys.py
+++ countingValleys.py
@@ -2,18 +2,20 @@
 Task from hakkerrank.com
 '''
 
-def countingValleys(n, s):
+def countingValleys(s): #missing n-argument!
     #WRONG!
     way = []
-	seaLevel = 0
-	for i in s:
-		if i == "U": #and seaLevel < 0?
-			seaLevel+=1
-			way.append(seaLevel)
-		elif i == "D": #and seaLevel <= 0?
-			seaLevel-=1
-			way.append(seaLevel)
-	return way.count(0)
+    seaLevel = 0
+    Valleys = 0
+    for i in s:
+        if i == "U": #and seaLevel < 0?
+            seaLevel+=1
+            way.append(seaLevel)
+        elif i == "D":
+            seaLevel-=1
+            way.append(seaLevel)
+    
+    return way
 
 
 
@@ -28,8 +30,8 @@ def countingValleys(n, s):
 def tests():
     s1 = ["D","D","U","U","U","U","D","D"]
     s2 = ["U","D","D","D","U","D","U","U"]    
-    print ('{} should be 2.'.format(countingValleys(p1))
-    print ('{} should be 1.'.format(countingValleys(p2))
+    print ('{} should be 2.'.format(countingValleys(s1)))
+    print ('{} should be 1.'.format(countingValleys(s2)))
 
 def main ():
     tests ()
