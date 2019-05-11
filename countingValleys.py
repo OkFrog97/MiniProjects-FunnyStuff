68572a417499e8b379e81e9874a158d3234a8cf7
OkForg97 <okfrog97@yandex.ru>
2019-05-11 09:31:29 +0500

    valleycounter

---
+++ countingValleys.py
@@ -0,0 +1,39 @@
+'''
+Task from hakkerrank.com
+'''
+
+def countingValleys(n, s):
+    #WRONG!
+    way = []
+	seaLevel = 0
+	for i in s:
+		if i == "U": #and seaLevel < 0?
+			seaLevel+=1
+			way.append(seaLevel)
+		elif i == "D": #and seaLevel <= 0?
+			seaLevel-=1
+			way.append(seaLevel)
+	return way.count(0)
+
+
+
+
+
+
+
+
+
+
+
+def tests():
+    s1 = ["D","D","U","U","U","U","D","D"]
+    s2 = ["U","D","D","D","U","D","U","U"]    
+    print ('{} should be 2.'.format(countingValleys(p1))
+    print ('{} should be 1.'.format(countingValleys(p2))
+
+def main ():
+    tests ()
+
+
+if __name__ == "__main__":
+    main()
