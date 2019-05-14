fc1c2c38313fdb0720c1d37378a6e64f3805fe8c
OkForg97 <okfrog97@yandex.ru>
2019-05-14 06:34:56 +0500

    play with C-lang functionc

---
+++ func_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int hi(){
+    printf("I am a function! ^_^\n");
+    return 0;
+}
+
+int main () {
+    printf("I am a body >_<\n");
+    hi ();
+    printf("I am a banana! (^0^)\n");
+    return 0;
+}
