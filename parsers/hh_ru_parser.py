a0fef5fb637d892e606f649dbe47310d2044d68b
OkForg97 <okfrog97@yandex.ru>
2019-05-17 06:06:00 +0500

    hh_parser begining

---
+++ parsers/hh_ru_parser.py
@@ -0,0 +1,42 @@
+#1/usr/bin/evn python3
+
+'''
+This program for pasrsing data from hh.ru
+'''
+
+def hh_parser(url):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test():
+    pass
+
+def main():
+    test()
+
+if __name__ == "__main__":
+    main()
