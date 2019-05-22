0c5cac29e034637f0dd5957accfdf173e188bcb8
OkForg97 <okfrog97@yandex.ru>
2019-05-22 06:40:19 +0500

    taks ok

--- python tasks/fib_mod.py
+++ python tasks/fib_mod.py
@@ -1,2 +1,10 @@
 def fib_mod (n, m):
- 
+    count = 0
+	f=[0,1]
+	for i in range (2,n+1):
+		temp = (f[i-1]+f[i-2])%m
+		f.append(temp)
+	    count ++ 1
+        if f[i] == 1 and f[i-1] ==0:
+            break
+    return f[n%count]
