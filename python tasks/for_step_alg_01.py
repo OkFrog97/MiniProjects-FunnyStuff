465c7caee1523e33c74e7bb31f6a74b07192e20e
OkForg97 <okfrog97@yandex.ru>
2019-05-25 23:37:03 +0500

    chek

--- python tasks/for_step_alg_01.py
+++ python tasks/for_step_alg_01.py
@@ -7,7 +7,7 @@ def checker():
     'math.log(math.log(n, 2))':math.log(math.log(n, 2)),
     'math.sqrt(math.log(n, 4))':math.sqrt(math.log(n, 4)),
     'math.log(n, 3)':math.log(n, 3),
-    'math.log(n, 2)' ** 2:math.log(n, 2) ** 2,
+    'math.log(n, 2) ** 2':math.log(n, 2) ** 2,
     'math.sqrt(n)':math.sqrt(n),
     'n / math.log(n, 5)':n / math.log(n, 5),
     'math.log(math.factorial(n),2)':math.log(math.factorial(n),2),
@@ -23,5 +23,8 @@ def checker():
     'math.factorial(n)':math.factorial(n)
     }
     
-    for i in solution:
-        print ('{0}:{1}'.format(i, solution[i])
+    for i in sorted(solution.items(), key= lambda x : x[1]):
+        print (i)
+
+if __name__ == '__main__':
+    checker()
