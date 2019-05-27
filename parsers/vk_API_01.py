fc5805de3082100a0cecf5102345c442be5e6a0e
OkForg97 <okfrog97@yandex.ru>
2019-05-27 07:18:23 +0500

    add basic functions: vk_parser, test, main

---
+++ parsers/vk_API_01.py
@@ -0,0 +1,41 @@
+'''
+Program for taking most interesting (more 500 likes) posts from FOOD PORN vk.com group.
+'''
+
+def vk_parser(vk_group=""):
+    #import modules
+    import requsts
+    from bs4 import beautifuleSoup as bs
+    
+    #system vars
+    service_key = '625109bd625109bd625109bdf2623bc2bc66251625109bd3eba4eb1c9ab68bbea9a3f17' #key from vk.com API
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def tests():
+    pass
+    
+
+
+def main()
+    pass
+    
+
+if __name__ == "__main__":
+    main()
