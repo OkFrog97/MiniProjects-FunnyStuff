4e9dd4101ce138b71cfcd472e6e2e142000b1434
OkForg97 <okfrog97@yandex.ru>
2019-06-02 10:27:34 +0500

    Add logging

--- parsers/vk_API_01.py
+++ parsers/vk_API_01.py
@@ -1,5 +1,5 @@
 '''
-Program for taking most interesting (more 500 likes) posts from FOOD PORN vk.com group.
+Programm for taking most interesting (more 500 likes) posts from vk.com groups.
 '''
 
 
@@ -7,27 +7,34 @@ def vk_parser(vk_group="fit4life_official"):
     '''
     Return posts from vk.com group.
     '''
-    # import modules
+    # import and initialization log module
+    import logging
+    logging.basicConfig(filename="vk_API_01.log", level=logging.DEBUG, format='%(asctime)s %(message)s')
+    logging.info("Loging module initialization: successfully")
+
+    # import programm modules
     import requests
     from bs4 import BeautifulSoup as bs
-    
-    # system vars
+    logging.info("Import modules: seccessfully")
+   
+   # system vars
     token = '625109bd625109bd625109bdf2623bc2bc66251625109bd3eba4eb1c9ab68bbea9a3f17' # service key from vk.com API
     version = 5.95
     offset = 0
-    count = 100
+    count = 1
     all_posts = []
     
     # getting data from vk
-    while offset < 100:
+    while offset < 1000:
         response = requests.get("https://api.vk.com/method/wall.get", 
                                 params={"access_token":token,
                                         "version":version,
                                         "domain":vk_group,
                                         "count":count,
                                         "offset":offset})
-    
-        data = response.json()['response']['items']
+        logging.debug("Response status: {}".format(response))
+        logging.debug("Response json: {}".format(response.json()))
+        data = response.json()["response"]["items"]
         all_posts.extend(data)
         offset += 10
     
