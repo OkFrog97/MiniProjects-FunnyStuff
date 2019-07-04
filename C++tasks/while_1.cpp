b8432ad13ea0a5c03f226a010a298760e2c8b821
OkForg97 <okfrog97@yandex.ru>
2019-07-04 07:20:38 +0500

    pup

--- C++tasks/while_1.cpp
+++ C++tasks/while_1.cpp
@@ -7,9 +7,14 @@ int main() {
   i = 1;
   
   while (x<N){
-  x = i*i;
-  cout << x << " ";
-  i++;
+      x = i*i;
+      if (x<N) {
+          cout << x << " ";
+      }
+      else {
+          break;
+      }
+      i++;
   }
   
   return 0;
