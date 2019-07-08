6f1a3f66f81957e87efc89af121cad1e7f8b43b7
OkForg97 <okfrog97@yandex.ru>
2019-07-08 05:08:58 +0500

    fix tab/spaces problem

--- python tasks/solution_2.py
+++ python tasks/solution_2.py
@@ -3,12 +3,12 @@ num_steps = int(sys.argv[1])
     
 def stair(n):    
     for i in range (n):
-		for j in range (n):
-			if j+1 < n-i:
-			    print (" ", end="")
-			else:
-			    print ("#", end="")
-		print()
+        for j in range (n):
+            if j+1 < n-i:
+                print (" ", end="")
+            else:
+                print ("#", end="")
+            print()
 
 stair (num_step)
-
+dsaxz
