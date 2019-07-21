111167ce90f8dd75855ea0a147b77847631304ac
OkForg97 <okfrog97@yandex.ru>
2019-07-21 10:22:18 +0500

    program is done

--- python tasks/files work/decoder.py
+++ python tasks/files work/decoder.py
@@ -4,17 +4,21 @@ def decode(dataset:str):
     >decode(a3B4f2)
     >aaaBBBBff
     '''
-    return "".join(dataset[i]*int(dataset[i+1]) for i in range(len(dataset)-1) if dataset[i].isalpha())
+    return "".join(dataset[i]*int(dataset[i+1]) for i in range(len(dataset)-1) if dataset[i].isalpha()) # if i not a number then char in next position(i+1) is a number
 
 
 def main():
-    
+    # open file with genome coding data
     with open ("dataset_3363_2.txt") as data:
        dataset = data.readline()
     
+    #decoding
     decoding_data = decode(dataset)
     
-    print(decoding_data)
+    #write decoding data in file
+    with open ("decode_data.txt", "w") as out:
+        out.write(decoding_data)
+    
     
 if __name__ == "__main__":
     main()
