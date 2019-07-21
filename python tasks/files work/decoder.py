c43d5875edd213d09ccbe670c9377dbc658918a1
OkForg97 <okfrog97@yandex.ru>
2019-07-21 10:09:06 +0500

    pre vers

--- python tasks/files work/decoder.py
+++ python tasks/files work/decoder.py
@@ -1,15 +1,32 @@
 def decode(dataset:str):
     '''
-    decode dataset from wordsnums to only words format.
+    decode genome_dataset from wordsNums_format to onlyWords_format.
     >decode(a3B4f2)
     >aaaBBBBff
     '''
     pass
+    
+    answer = ""
+    
+    for i in range(len(dataset)-1):
+        if dataset[i].isalpha():
+            answer += dataset[i]*int(dataset[i+1]
+    
+    return answer
+    
+    
 
 
 
-
-
+genome = input()+' '
+s = 0
+n=genome[0]
+for i in genome:       
+    if n!=i:
+        print(n + str(s), end = '')
+        s=0
+        n=i
+    s+=1
 
 
 
@@ -23,8 +40,6 @@ def main():
     
     with open ("dataset_3363_2.txt") as data:
        dataset = data.readline()
-    
-    print(type(dataset))
     
     decoding_data = decode(dataset)
     
