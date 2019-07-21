109443c92821745019f5b673523c204b228de911
OkForg97 <okfrog97@yandex.ru>
2019-07-21 09:52:45 +0500

    add empty functional

--- python tasks/files work/decoder.py
+++ python tasks/files work/decoder.py
@@ -1,6 +1,33 @@
+def decode(dataset:str):
+    '''
+    decode dataset from wordsnums to only words format.
+    >decode(a3B4f2)
+    >aaaBBBBff
+    '''
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
     
     with open ("dataset_3363_2.txt") as data:
-    dataset = data.readline()
+       dataset = data.readline()
     
-    print(dataset)
+    print(type(dataset))
+    
+    decoding_data = decode(dataset)
+    
+    
+if __name__ == "__main__":
+    main()
