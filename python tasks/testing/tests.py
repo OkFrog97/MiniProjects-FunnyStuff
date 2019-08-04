56d3250e2b07d310dbff6c5f580a0a7c0ecd7085
OkForg97 <okfrog97@yandex.ru>
2019-08-04 07:40:32 +0500

    our first test!

---
+++ python tasks/testing/tests.py
@@ -0,0 +1,12 @@
+from name_function import get_formated_name
+import unittest
+
+class test(unittest.TestCase):
+    
+    def test_first_last_name(self):
+        formated_name = get_formated_name("jenis", "joplin")
+        self.assertEqual(formated_name, "Jenis Joplin")
+
+
+unittest.main()
+    
