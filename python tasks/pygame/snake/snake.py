7ef1f27c00df91aab90d4451ce46831188695dd3
OkForg97 <okfrog97@yandex.ru>
2019-08-04 11:42:33 +0500

    asd

--- python tasks/pygame/snake/snake.py
+++ python tasks/pygame/snake/snake.py
@@ -61,44 +61,108 @@ class Game():
         game.fps_controller.tick(23)
         
     def show_score(self, choice=1):
-        s_font = pygame.font.
+        """Отображает результат"""
+        s_font = pygame.font.SysFont('monaco', 24)
+        s_surf = s_font.render("Score: {0}".format(self.score), True, self.black)
+        s_rect = s_surf.get_rect()
         
+        if choice == 1:
+            s_rect.midtop(80, 10)
+        else:
+            s.rect.midtop(360, 120)
         
+        self.play_surface.blit(s_surf, s_rect)
         
+    
+    def game_over(self):
+        """Выводит результат и надпись геймовер в случае выхода"""
+        go_font = pygame.font.SysFont('monaco', 24)
+        go_surf = go_font.render.('Game over', True, self.red)
+        gp_rect = go_surf.get_rect()
+        go_rect.midtop(360, 15)
+        self.play_surface.blit(go_surf, go_rect)
+        self.show_score(0)
+        pygame.display.flip()
+        time.sleep(3)
+        pygame.quit()
+        sys.exit()
+
+
+class Snake():
+    
+    def __init__(self, snake_color):
+        self.snake_head_pos = [100, 50] #Позиция змеиной головушки
+        self.snake_body_pos = [[100, 50],[90, 50],[80,50]]
+        self.snake_color = snake_color
+        self.direction = "RIGHT"
+        self.change_to = self.direction
+    
+    def snake_body_mechanism(self, score, food_pos, screen_wedth, screen_height):
+        """Механизм работы тела змейки"""
         
+        self.snake_body.insert(0, list(self,snake_head_pos))
         
+        # Eat the food
+        if (self.snake_head_pos[0] == food_pos [0] and self.snake_head_pos[1] == food_pos[1]):
+            food_pos = [random.randrange(1,screen_wedth/10)*10, random.randrange(1,screen_height/10)*10]
+            score += 1
         
+        else:
+            self.snake_body.pop()
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return score, food_pos
+    
+    def draw_snake(self, play_surface, surface_color):
+        play_surface.fill(surface_color)
+        for pos in self.snake_body:
+            pygame.draw.rect(play_surface, self.snake_color, pygame.Rect(pos[0], pos[1], 10, 10))
+    
+    def check_for_boundaries(self, game_over, screen_wedth, screen_height):
+    
+    if any((
+        self.snake_head_pos[0] > screen_wedth-10 or
+        self.snake_head_pos[0] < 0,
+        self.snake_head_pos[1] > screen_height-10 or
+        self.snake_head_pos[1] < 0
+        )):
+        game_over()
+    
+    for block in self.snake_body[1:]:
+        if (block[0] == self.snake_head_pos[0] and block [1] == self.snake_head_pos[1]):
+            game_over()
+    
+    
+class Food():
+    
+    def __init__(self, food_color, screen_wedth, screen_height):
+        self.food_color = food_color
+        self.food_size_x = 10
+        self.food_size_y = 10
+        self.food_pos = [random.randrange(1,screen_wedth/10)*10, random.randrange(1,screen_height/10)*10]
+    
+    def food_draw(self):
+        pygame.draw.rect(play_surface, self.food_color, pygame.Rect(pos[0], pos[1], 10, 10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main ()    
+    
