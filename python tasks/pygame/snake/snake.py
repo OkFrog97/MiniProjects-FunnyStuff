e3b93e73d7bdc7983495fbb6530cb314f47b478c
OkForg97 <okfrog97@yandex.ru>
2019-08-04 11:53:15 +0500

    snake is ready for first start

--- python tasks/pygame/snake/snake.py
+++ python tasks/pygame/snake/snake.py
@@ -144,23 +144,32 @@ class Food():
         pygame.draw.rect(play_surface, self.food_color, pygame.Rect(pos[0], pos[1], 10, 10))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def main():
-    pass
+   
+    # Объявляем циклы.
+    game = Game()
+    snake = Snake(game.green)
+    food = Food(game.brown, game.screen_wedth, game.screen_height)
+   
+    game.init_and_check_for_errors()
+    game.set_surface_and_title() #!!!!
+    
+    while True:
+        snake.change_to = game.event_loop(snake.change_to)
+        
+        snake.validation_direction_and_change()
+        snake.change_head_position()
+        
+        game.score, food.food_pos = snake.snake_body_mechanism(game.score, food.food_pos, game.screen_width, game.screen_height)
+        snake.draw_snake(game.play_surface, game.white)
+        
+        food.draw_food(game.play_surface)
+
+        snake.check_for_boundaries(game.game_over, game.screen_width, game.screen_height)
+
+        game.show_score()
+        game.refresh_screen()
+
 
 
 if __name__ == "__main__":
