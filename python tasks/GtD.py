357d211677beae63fdebd1c671e6bc07f4f94669
OkForg97 <okfrog97@yandex.ru>
2019-08-20 05:52:15 +0500

    bribradra

--- python tasks/GtD.py
+++ python tasks/GtD.py
@@ -1,4 +1,23 @@
+import time
+
+
+class Loggable:
+    def log(self, msg):
+        print(str(time.ctime()) + ": " + str(msg))
+        
+
 class LoggableList(list, Loggable):
     def append(self, p_object):
         self.log(p_object)
         super().append(p_object)
+
+
+def test():
+    t = LoggableList()
+    t.append(1)
+    t.append('str')
+    assert len(t) == 2
+
+
+if __name__ == "__main__":
+    test()
