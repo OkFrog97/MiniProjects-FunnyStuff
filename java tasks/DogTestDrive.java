487b67615d53b5917ffe6bf5f4e27ee1cc07bd91
OkForg97 <okfrog97@yandex.ru>
2019-09-21 19:46:23 +0500

    dog do doggy things

---
+++ java tasks/DogTestDrive.java
@@ -0,0 +1,17 @@
+class Dog {
+    int size;
+    String breed;
+    String name;
+    
+    void bark() {
+        System.out.println("Bow! WOw!");
+    }
+
+class DogTestDrive {
+    public static void main(String[] args){
+    // Проверочный код для класса DogTestDrive    
+        Dog bim = Dog();
+        d.size = 40;
+        s.bark();
+    }
+}
