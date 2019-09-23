cd0bffa9c6c7cd539cf760f669adb40d23519f7c
OkForg97 <okfrog97@yandex.ru>
2019-09-23 07:12:36 +0500

    bot_repeater test1

--- bots/Bot_repiter/bot.py
+++ bots/Bot_repiter/bot.py
@@ -1,10 +1,19 @@
-# Simple telegram bor, which repeat your message in telegramm
+# Simple telegram bor, which repeat your message in telegram
 # Name: test_bot;
 # Username: test23092019_bot
+
+# import other files and libs
+import setting
+import telebot
 
 # add token
 token = "942108158:AAEvqWvMWSmhBXks5s_n7La2npUnbZVr6ds"
 
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
