5308bbfc69c5bb938206576e51d35f5212ad1ea4
OkForg97 <okfrog97@yandex.ru>
2019-09-24 06:13:11 +0500

    try do it with classes

---
+++ java tasks/dogArr.java
@@ -0,0 +1,17 @@
+class dogArr{
+    String name;
+    
+    public static void main (String[] args){
+        
+        dogArr [] d = new dogArr[3];
+        
+        d[0] = dogArr();
+        d[1] = dogArr();
+        d[0].name = "Fido";
+        
+        System.out.println(d[0]);
+        System.out.println(d[0].name);
+        System.out.println(d[1].name);
+        System.out.println(d[2]);
+    }
+}        
