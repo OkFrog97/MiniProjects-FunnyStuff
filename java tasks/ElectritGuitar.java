f5f38ac7297116c661bdb31acc0ceecf2ff8c0fc
OkForg97 <okfrog97@yandex.ru>
2019-12-30 14:16:54 +0500

    new day new code

---
+++ java tasks/ElectritGuitar.java
@@ -0,0 +1,21 @@
+class ElectricGuitar {
+    
+    //vara
+    String band;
+    int numOfPickups;
+    boolean rockStarUsesIt;
+    
+    //methods
+    String getBrand(){};
+    
+    void SetBand(String aBrand){};
+    
+    int getNumberOfPickups(){};
+    
+    void setNumberOfPIckuos(){};
+
+    boolean getRockStarUsesIt(){};
+    
+    void setRockStarUsesIt(){};    
+
+}
